5eef6abaab6d9e1a1196c82ac9637628fdc27341
Rafee <mdrafee03@gmail.com>
2020-03-19 20:37:48 +0600

    code cleaning

--- src/app/chip-field/chip-field.module.ts
+++ src/app/chip-field/chip-field.module.ts
@@ -1,23 +1,23 @@
-import { MatChipsModule } from '@angular/material/chips';
-import { MatSelectModule } from '@angular/material/select';
-import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatChipsModule } from '@angular/material/chips';
 import { ChipFieldComponent } from './chip-field/chip-field.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ChipFieldComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule,
-    MatSelectModule,
-    MatFormFieldModule,
-    MatChipsModule,
     MatAutocompleteModule,
-    MatIconModule
+    MatChipsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatSelectModule,
+    ReactiveFormsModule
   ],
   exports: [ChipFieldComponent]
 })
